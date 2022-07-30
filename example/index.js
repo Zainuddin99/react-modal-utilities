1cccdb36b11ffea2395d9a948fce9c2f44dcdd97
Zainuddin99 <zainuzayn11@gmail.com>
2022-07-30 22:34:22 +0530

    fix: responsiveness fixed

--- example/index.js
+++ example/index.js
@@ -1,18 +1,18 @@
 // index.js
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
-import { Modal, useModalState } from '../.'
+import {Modal} from '../.'
 
 const App = () => {
-    const [isOpen, setOpen] = useState(false)
+    const [isOpen, setOpen] = useState(true)
 
     return <div>
-        <button onClick={() => setOpen(true)}>Open</button>
+        <button onClick={()=>setOpen(true)}>Open</button>
         {
-            isOpen && <Modal backdrop={false} close={() => setOpen(false)}>
+            isOpen && <Modal label="random" close={()=>setOpen(false)} animate="pop-down">
                 {
-                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(item=>(<div key={item}>
-                        <h1>Hey how are you guys this is sooo cool ha so what ups ha </h1>
+                    [1].map(item=>(<div key={item}>
+                        <h1>Hey </h1>
                     </div>))
 
                 }
